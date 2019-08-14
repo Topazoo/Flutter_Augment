74da4763b754e731e9c899e47e80dad882488e2f
Peter Swanson <topazoo42@gmail.com>
2019-08-13 23:23:17 -0700

    Allows lighting models to be applied

--- lib/Models/Base/graphics.dart
+++ lib/Models/Base/graphics.dart
@@ -7,6 +7,8 @@ part of AR_Models_Library;
       Methods:
         - apply_texture(): Apply an image or color to the model. Images are
                            loaded from /assets/images/.
+        - apply_lighting(): Apply lighting to the model. Options are:
+                            lambert, blinn, constant, phong and physical.
 */
 
 mixin AR_Model_Graphics {
@@ -18,9 +20,28 @@ mixin AR_Model_Graphics {
     model.node.geometry.materials.notifyListeners();
   }
 
-  void apply_lighting({AR_Model model})
+  void apply_lighting({AR_Model model, String lighting})
   {
-    //ARKitMaterial new_material = new ARKitMaterial()
     /* Apply lighting to the model */
+
+    ARKitMaterial new_lighting;
+
+    if(lighting == 'lambert')
+      new_lighting = ARKitMaterial(lightingModelName: ARKitLightingModel.lambert); 
+    else if(lighting == 'blinn')
+      new_lighting = ARKitMaterial(lightingModelName: ARKitLightingModel.blinn);
+    else if(lighting == 'constant')
+      new_lighting = ARKitMaterial(lightingModelName: ARKitLightingModel.constant);
+    else if(lighting == 'phong')
+      new_lighting = ARKitMaterial(lightingModelName: ARKitLightingModel.phong);
+    else if(lighting == 'physical')
+      new_lighting = ARKitMaterial(lightingModelName: ARKitLightingModel.physicallyBased);
+
+    if(new_lighting != null)
+    {
+      model.node.geometry.materials.value.add(new_lighting);
+      model.node.geometry.materials.notifyListeners();
+    }
+
   }
 }
